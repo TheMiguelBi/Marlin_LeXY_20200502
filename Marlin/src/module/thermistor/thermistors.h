db35b9beb480728947cc761c72bb12f6bb65336b
TheMiguelBi <31853557+TheMiguelBi@users.noreply.github.com>
2020-05-02 16:49:55 +0200

    Update thermistors.h

--- Marlin/src/module/thermistor/thermistors.h
+++ Marlin/src/module/thermistor/thermistors.h
@@ -109,6 +109,12 @@
 #if ANY_THERMISTOR_IS(21) // Pt100 with INA826 amp with 3.3v excitation based on "Pt100 with INA826 amp on Ultimaker v2.0 electronics"
   #include "thermistor_21.h"
 #endif
+#if ANY_THERMISTOR_IS(22) // TERMISTOR 22 - Amplificatore E3D PT100
+  #include "thermistor_22.h"
+#endif
+#if ANY_THERMISTOR_IS(24) // TERMISTOR 24 - Amplificatore E3D PT100
+  #include "thermistor_24.h"
+#endif
 #if ANY_THERMISTOR_IS(51) // beta25 = 4092 K, R25 = 100 kOhm, Pull-up = 1 kOhm, "EPCOS"
   #include "thermistor_51.h"
 #endif
