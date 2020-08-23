1890e14b7178f79852908e2e34b4867eb4011999
TheMiguelBi <31853557+TheMiguelBi@users.noreply.github.com>
2020-08-23 13:42:36 +0200

    Add PIDTEMPBED value

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -538,11 +538,16 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
+  //Lexy CoreXY
+  #define DEFAULT_bedKp 87.44
+  #define DEFAULT_bedKi 16.82
+  #define DEFAULT_bedKd 303.12
+
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
